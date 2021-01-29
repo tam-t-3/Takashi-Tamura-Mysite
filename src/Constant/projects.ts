da5771184aba9f6f46b3e7fb8c7ae4a7bb7f1fbe
田村 充 <takashi.tamura@tamuranoAir.elecom>
2021-01-29 15:33:18 +0900

    add Github url

--- src/Constant/projects.ts
+++ src/Constant/projects.ts
@@ -4,21 +4,27 @@ import GraphApp from "../assets/images/graph.jpg";
 // eslint-disable-next-line import/prefer-default-export
 export const works = [
   {
-    title: "Population-Graph-App",
+    title: "Popuraph",
     description: "各国の人口数をReact / Rechartsで表示するアプリ",
     thumb: GraphApp,
     url: "https://basic-population.web.app",
+    github: "Github",
+    githubUrl: "https://github.com/Takashi-Tamura/Graph-App-Prd",
   },
   {
     title: "Undeveloped",
     description: "Explanation",
     thumb,
     url: "",
+    github: "",
+    githubUrl: "",
   },
   {
-    title: "開発中",
-    description: "開発中",
+    title: "Undeveloped",
+    description: "Explanation",
     thumb,
     url: "",
+    github: "",
+    githubUrl: "",
   },
 ];
